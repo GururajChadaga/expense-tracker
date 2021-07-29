6af8abd390782c995b4461c17af3514b12b63b87
gururajchadaga <guruuraj.chadaga@gmail.com>
2021-07-29 23:11:36 +0530

    ExpenseItems component: Made reusable with fake dynamic data

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,5 +1,15 @@
-function ExpenseItems() {
-  return <h2>Expense Item!</h2>;
+import "./ExpenseItems.css";
+function ExpenseItems(props) {
+
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItems;
